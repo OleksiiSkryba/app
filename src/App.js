d5bee47887c1b57a5e18ddf75b5a45c930c89996
Oleksii Skryba <Oleksii.Skryba@Gmail.Com>
2024-08-01 15:21:02 +0200

    Merge pull request #1 from OleksiiSkryba/hw1
    start

--- src/App.js
+++ src/App.js
@@ -2,11 +2,20 @@ import './App.css';
 import {useState} from 'react';
 import Users from './components/users/Users';
 import User from "./components/user/User";
+
 export default function App() {
+
   let [user, setUser] = useState({});
+
+  const [value, setValue] = useState("I am edittable");
+  const onChange = (e) => {
+    const html = e.target.innerHTML;
+    setValue(html);
+  };
+
   return (
       <div className={'App'}>
-          <h1 className={'header'}>hw2</h1>
+          <h1 className={'header'}onChange={onChange} contentEditable>{value}</h1>
         <Users setUser={setUser}/>
           <User item={user}/>
 
